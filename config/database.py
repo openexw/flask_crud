4b636804ceeb216c931637e01fd7e00ab9384447
yixue <yixue@yixuedeMacBook-Pro-3.local>
2019-04-04 12:18:56 +0800

    add .gitinore file

--- config/database.py
+++ config/database.py
@@ -3,4 +3,5 @@ Created by ranml on 2019/1/28
 """
 __author__ = 'ranml'
 
-SQLALCHEMY_DATABASE_URI = "mysql+cymysql://homestead:secret@192.168.10.10:3306/rml_final_design"
+# SQLALCHEMY_DATABASE_URI = "mysql+cymysql://homestead:secret@192.168.10.10:3306/rml_final_design"
+SQLALCHEMY_DATABASE_URI = "mysql+cymysql://root:123456@127.0.0.1:3306/crud"
