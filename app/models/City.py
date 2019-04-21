26fd798215d845ce7fa007978cd0ebe8b574db35
ranjun <ranjun@songshuai.com>
2019-04-21 20:22:23 +0800

    add city model

--- app/models/City.py
+++ app/models/City.py
@@ -1,9 +1,14 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String,SMALLINT
 
 from app.models.Base import Base
 
 
 class City(Base):
     id = Column(Integer, primary_key=True)
-    # name
-
+    name = Column(String(30))
+    pid = Column(Integer)
+    deep = Column(SMALLINT)
+    pinyin_prefix = Column(String(100))
+    pinyin = Column(String(120))
+    ext_id = Column(String(50))
+    ext_name = Column(String(100))
