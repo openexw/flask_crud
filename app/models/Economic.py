c0a645ed0bb2765c3fecf0adf156e92340ed66e7
ranml <1376680150@qq.com>
2019-05-03 00:04:34 +0800

    asas

--- app/models/Economic.py
+++ app/models/Economic.py
@@ -6,7 +6,7 @@ class Economic(Base):
     id = Column(Integer(11), primary_key=True)
     eco_name = Column(VARCHAR(24), nullable=False)
     eco_unit = Column(VARCHAR(10),nullable=False)
-    city_id = Column(Integer(11), primary_key=True)
+    city_id = Column(Integer(11), primary_key=False)
     data = Column(Integer(20),nullable=False)
 
 
